8afbeed962cc609fabc5421a3bc636383bb315fa
demondamz <74813656+demondamz@users.noreply.github.com>
2021-11-25 15:53:45 +0000

    changes

--- PGPMM-install.sh
+++ PGPMM-install.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-plexmeta
-function plexmeta() {
+#bin/bash 
+function plexmeta(){
     sudo git clone --quiet https://github.com/demondamz/PG-Plex-Meta-Manager.git /opt/plex-meta
     cd /opt/plex-meta
     sudo ansible-playbook ./plex-meta-manager.yml
@@ -8,13 +7,13 @@ function plexmeta() {
     mv /opt/plex-meta/movies.yml /opt/appdata/plex-meta-manager/app/movies.yml
     mv /opt/plex-meta/tv.yml /opt/appdata/plex-meta-manager/app/tv.yml
     cd /opt/appdata/plex-meta-manager
-    rm -rf /opt/plex-meta 
+    rm -rf /opt/plex-meta
     sudo chown -cR 1000:1000 /opt/pgclone/ 1>/dev/null 2>&1
-    sudo chmod -cR 755 /opt/pgclone 1>/dev/null 2>&1	
-    
+    sudo chmod -cR 755 /opt/pgclone 1>/dev/null 2>&1
     tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ⛔️  Please stop the Container & Edit the config. 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
 }
+plexmeta
