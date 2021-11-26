cc087c4d4e4615937258a978f2c5d8d4a787bcb1
demondamz <74813656+demondamz@users.noreply.github.com>
2021-11-26 10:57:44 +0000

    Update PGPMM-install.sh

--- PGPMM-install.sh
+++ PGPMM-install.sh
@@ -3,9 +3,9 @@ function plexmeta(){
     sudo git clone --quiet https://github.com/demondamz/PG-Plex-Meta-Manager.git /opt/plex-meta
     cd /opt/plex-meta
     sudo ansible-playbook ./plex-meta-manager.yml
-    cp /opt/plex-meta/config.yml /opt/appdata/plex-meta-manager/app/config.yml
-    cp /opt/plex-meta/Movies.yml /opt/appdata/plex-meta-manager/app/Movies.yml
-    cp /opt/plex-meta/TV.yml /opt/appdata/plex-meta-manager/app/TV.yml
+    cp ./config.yml /opt/appdata/plex-meta-manager/app/config.yml
+    cp ./Movies.yml /opt/appdata/plex-meta-manager/app/Movies.yml
+    cp ./TV.yml /opt/appdata/plex-meta-manager/app/TV.yml
     cd /opt/appdata/plex-meta-manager
     rm -rf /opt/plex-meta
     sudo chown -cR 1000:1000 /opt/pgclone/ 1>/dev/null 2>&1
